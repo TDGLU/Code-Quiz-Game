de91b0054a3cc8a3279c1d35d9d8c92b17c9e476
TDGLU <tdglukealt@gmail.com>
2022-10-06 15:29:06 -0700

    relinked the images

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
 console.log('Test')
 
-var question = document.getElementById('question')
-var c1 = document.getElementById('c1')
-var c2 = document.getElementById('c2')
-var c3 = document.getElementById('c3')
-var c4 = document.getElementById('c4')
+let question = document.getElementById('question')
+let c1 = document.getElementById('c1')
+let c2 = document.getElementById('c2')
+let c3 = document.getElementById('c3')
+let c4 = document.getElementById('c4')
 
