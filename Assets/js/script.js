a43cbb31368824e5f397810078805e185a443479
TDGLU <tdglukealt@gmail.com>
2022-10-06 14:43:22 -0700

    added ids to all choices and question, changed frog img to be more compressed, and centered the choices in css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,8 @@
 console.log('Test')
+
+var question = document.getElementById('question')
+var c1 = document.getElementById('c1')
+var c2 = document.getElementById('c2')
+var c3 = document.getElementById('c3')
+var c4 = document.getElementById('c4')
+
