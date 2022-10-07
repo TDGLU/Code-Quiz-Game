98101d5eefc452b6530afc595b1146d373c09a9c
TDGLU <tdglukealt@gmail.com>
2022-10-06 17:59:59 -0700

    added hide class to hide start button when clicked

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,25 @@
 console.log('Test')
 
-let question = document.getElementById('question')
 let c1 = document.getElementById('c1')
 let c2 = document.getElementById('c2')
 let c3 = document.getElementById('c3')
 let c4 = document.getElementById('c4')
 
+let question = document.getElementById('question')
+
+const startButton = document.getElementById('start')
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+  console.log('Started')
+  startButton.classList.add('hide')
+}
+
+function nextQuestion() {
+
+}
+
+function answer() {
+
+}
